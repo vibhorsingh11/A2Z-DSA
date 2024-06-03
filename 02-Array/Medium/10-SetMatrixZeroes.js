e580b45b8dc432e7efa243421f5227d288528f6b
vibhorsingh11 <vibhorsinghcs@gmail.com>
2024-06-03 13:19:36 +0530

    Added Set Matrix Zeroes

---
+++ 02-Array/Medium/10-SetMatrixZeroes.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+  let row = matrix.length;
+  let column = matrix[0].length;
+  let col0 = 1;
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < column; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0;
+        if (j !== 0) {
+          matrix[0][j] = 0;
+        } else {
+          col0 = 0;
+        }
+      }
+    }
+  }
+
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 1; j < matrix[0].length; j++) {
+      if (matrix[i][j] !== 0) {
+        if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+  }
+
+  if (matrix[0][0] === 0) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+
+  if (col0 === 0) {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+
+  return matrix;
+};
+
+console.log(
+  setZeroes([
+    [0, 1, 2, 0],
+    [3, 4, 5, 2],
+    [1, 3, 1, 5],
+  ])
+); //[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+//TC: O(2MN) SC: O(1)
