9892a3877dc7814697d6af557e6905c39dc8634c
vibhorsingh11 <vibhorsinghcs@gmail.com>
2024-06-12 13:36:57 +0530

    Added Binary Search

---
+++ 03-BinarySearch/01-BinarySearch.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  const n = nums.length;
+  let left = 0,
+    right = n - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9)); //4
+
+//TC: O(logN) SC: O(1)
