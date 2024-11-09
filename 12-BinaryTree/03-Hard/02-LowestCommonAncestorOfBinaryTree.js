8762215537932a159d8a7d495b831dea19b4e1b2
vibhorsingh11 <vibhorsinghcs@gmail.com>
2024-11-09 15:43:04 +0530

    Added Lowest common ancestor

---
+++ 12-BinaryTree/03-Hard/02-LowestCommonAncestorOfBinaryTree.js
@@ -0,0 +1,42 @@
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  if (root === null || p === root || q === root) {
+    return root;
+  }
+
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
+  if (left === null) return right;
+  else if (right === null) return left;
+  else {
+    return root;
+  }
+};
+
+class TreeNode {
+  constructor(val = 0, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+let root = new TreeNode(3);
+root.left = new TreeNode(5);
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(6);
+root.left.right = new TreeNode(2);
+root.left.right.left = new TreeNode(7);
+root.left.right.right = new TreeNode(4);
+root.right.left = new TreeNode(0);
+root.right.right = new TreeNode(8);
+
+console.log(lowestCommonAncestor(root, 5, 1)); // 3
+
+//TC: O(N)
+//SC: O(N) height of the tree
