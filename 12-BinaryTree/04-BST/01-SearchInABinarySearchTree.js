53ce36dddaa114e079b405084f92f74050d32302
vibhorsingh11 <vibhorsinghcs@gmail.com>
2024-12-15 17:49:55 +0530

    Added Search in a Binary Search Tree

---
+++ 12-BinaryTree/04-BST/01-SearchInABinarySearchTree.js
@@ -0,0 +1,27 @@
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function (root, val) {
+  while (root !== null && root.val !== val) {
+    root = root.val < val ? root.right : root.left;
+  }
+  return root;
+};
+
+class TreeNode {
+  constructor(val = 0, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+let root = new TreeNode(4);
+root.left = new TreeNode(2);
+root.right = new TreeNode(7);
+root.left.left = new TreeNode(1);
+root.left.right = new TreeNode(3);
+
+console.log(searchBST(root, 2).val);
